8c7083324d52234bf1cad649f2a3301b59bd29b8
Recnoa <106403650+Recnoa@users.noreply.github.com>
2022-06-02 00:57:40 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,16 +1,21 @@
-import discord
 from discord.ext import commands
+from os import getenv
+import traceback
 
-TOKEN = "your-token-here"
-bot = commands.Bot(command_prefix="!")
+bot = commands.Bot(command_prefix='/')
 
-#bot起動完了時に実行される処理
+
 @bot.event
-async def on_ready():
-    print('ログイン成功')
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
 
-#メッセージ受信時に実行される処理
-@bot.event
-async def on_message(message):
-    #on_messageイベントの取得とコマンド機能を併用する際に必要な処理
-    await bot.process_commands(message)
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+
+token = getenv('DISCORD_BOT_TOKEN')
+bot.run(token)
